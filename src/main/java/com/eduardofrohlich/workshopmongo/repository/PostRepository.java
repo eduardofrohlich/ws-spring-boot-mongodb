1a03ab83dc5bad7bc4041b799501f4257cf353da
eduardofrohlich <eduardofrohlich121@gmail.com>
2023-12-24 09:28:41 -0300

    searchtext with @query

--- src/main/java/com/eduardofrohlich/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/eduardofrohlich/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.eduardofrohlich.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.eduardofrohlich.workshopmongo.domain.Post;
@@ -10,6 +11,11 @@ import com.eduardofrohlich.workshopmongo.domain.Post;
 @Repository 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	// ?0 pois eh apenas um parametro (String text)
+	// i - ignora maiusculas ou minusculas
+		@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+		List<Post> searchTitle(String text);
+	
 		List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
